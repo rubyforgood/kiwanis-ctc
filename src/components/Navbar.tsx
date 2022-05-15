e274ee75a10b7f8a10466fe6865cb96eb9821757
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-05-15 21:00:02 +0200

    Removed unused import statements and did some cleanup of the code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,9 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { Link } from "react-router-dom";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
@@ -36,21 +32,13 @@ const ResponsiveAppBar = () => {
 	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 	const [activeOne, setActiveOne] = useState("");
 
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
 	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorElUser(event.currentTarget);
-	};
-
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
 	};
 
 	const handleCloseUserMenu = () => {
 		setAnchorElUser(null);
 	};
-
 
 	const handleLogoReset = () => {
 		setActiveOne("home");
