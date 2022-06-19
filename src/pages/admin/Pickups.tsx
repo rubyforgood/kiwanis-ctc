228420a2c2ab8b89b06292f2ddd1c20b397979b2
AhadAli01 <ahadali101aa@gmail.com>
2022-06-19 09:23:04 -0600

    Fixed the spacing/padding on the pickups page

--- src/pages/admin/Pickups.tsx
+++ src/pages/admin/Pickups.tsx
@@ -57,9 +57,9 @@ function getComparator<Key extends keyof any>(
 	order: Order,
 	orderBy: Key,
 ): (
-		a: { [key in Key]: number | string },
-		b: { [key in Key]: number | string },
-	) => number {
+	a: { [key in Key]: number | string },
+	b: { [key in Key]: number | string },
+) => number {
 	return order === "desc"
 		? (a, b) => descendingComparator(a, b, orderBy)
 		: (a, b) => -descendingComparator(a, b, orderBy);
@@ -381,8 +381,23 @@ export default function Pickups() {
 		}
 	};
 
+
+	useEffect(() => {
+		const reloadCount: string = sessionStorage.getItem("reloadCount")!;
+		console.log(reloadCount);
+		if (reloadCount === null) {
+			sessionStorage.setItem("reloadCount", String(1));
+			window.location.reload();
+		} else if (parseInt(reloadCount) < 2) {
+			sessionStorage.setItem("reloadCount", String(reloadCount + 1));
+			window.location.reload();
+		} else {
+			sessionStorage.removeItem("reloadCount");
+		}
+	}, []);
+
 	return (
-		<Box sx={{display: "flex"}}>
+		<Box sx={{ display: "flex" }}>
 			<AdminTaskbar />
 			<Box sx={{ width: "85%", float: "right" }}>
 				<Box sx={{ pl: 5 }}>
