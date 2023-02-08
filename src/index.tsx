a91fed45dc0b377445d3a37a6173c4d308771f85
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-08 01:44:32 -0500

    fix ReactDOM deprecation issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
+//External Modules
 import * as React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+
+//Internal Modules
 import App from "./App";
 import theme from "./theme";
 
-ReactDOM.render(
+
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
+root.render(
 	<ThemeProvider theme={theme}>
 		{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 		<CssBaseline />
 		<App />
 	</ThemeProvider>,
-	document.querySelector("#root"),
 );
