b336e87f9d7b28662425ec6cdcc3449bb755f477
Md Abdullah Mehedi Patwary <abdullahmehedi000@gmail.com>
2023-02-18 16:36:49 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import Dashboard from "./components/dashboard/Dashboard";
+import Pickup from "./components/pickup/Pickup";
 
 export default function App() {
 	return (
-		<Dashboard />
+		// <Dashboard />
+		<Pickup/>
 	);
 }
---
+++ src/components/pickup/Pickup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Pickup() {
+	return (
+		<div id = 'page-route'>Dashboard/Pickups</div>
+	);
+}
+
+export default Pickup;
