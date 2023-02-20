229ccf99e992b99acd9189a6ab7b65752e607de1
Cole Thiessen <colekthiessen@gmail.com>
2023-02-20 15:56:13 -0700

    fix: linting issues

--- src/theme.ts
+++ src/theme.ts
@@ -2,23 +2,23 @@ import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#21315C",
-		},
-		secondary: {
-			main: "#E8C887",
-		},
-		success: {
-			main: "#E3EECB",
-		},
-		error: {
-			main: "#FFD0CA",
-		},
-		info: {
-			main: "#FFF0CB",
-		},
-	},
+    palette: {
+        primary: {
+            main: "#21315C",
+        },
+        secondary: {
+            main: "#E8C887",
+        },
+        success: {
+            main: "#E3EECB",
+        },
+        error: {
+            main: "#FFD0CA",
+        },
+        info: {
+            main: "#FFF0CB",
+        },
+    },
 });
 
 export default theme;
