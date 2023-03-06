e843dd12323924969358f3037027cefffae016a9
Timi <alabitimi65@gmail.com>
2023-03-05 21:19:35 -0700

    fix: indentation eslint fix

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 
 export default function Login() {
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				width: 300,
-				height: 300,
-				backgroundColor: "primary.dark",
-			}}
-		/>
-	);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: 300,
+        height: 300,
+        backgroundColor: "primary.dark",
+      }}
+    />
+  );
 }
