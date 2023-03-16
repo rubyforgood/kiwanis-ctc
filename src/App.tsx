b28292725c1e867b9864f1960eda72ab52fb6e4c
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2023-03-16 21:56:37 +0000

    refactor: Merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { Dashboard } from "./components/dashboard/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
 
 export default function App() {
     return (
-        <Dashboard>
-        </Dashboard>
+        <Dashboard />
     );
 }
