900034fac83477e58f2d6f0b7fa1d6aac9076a2d
SBrazzCode <brazzoni12@hotmail.com>
2023-05-20 22:02:43 -0600

    feat: style pickups page

--- src/components/pickup/Pickups.tsx
+++ src/components/pickup/Pickups.tsx
@@ -1,32 +1,46 @@
 import React from "react";
 import Tabs from "./Tabs";
 import Box from "@mui/system/Box";
+import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
 import { Order } from "../../types/Order";
 
 
 export default function Pickups({ orders }: { orders: Order[] }) {
     return (
         <Box
+            component='main'
             sx={{
-                paddingLeft: "20px",
-                paddingRight: "20px",
-                display: "flex",
-                flexDirection: "column",
-                flex: "1",
+                backgroundColor: (theme) =>
+                    theme.palette.mode === "light"
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                flexGrow: 1,
                 height: "100vh",
-                fontFamily: "Avenir Next",
+                overflow: "auto",
+                p: 2,
             }}
         >
-            Dashboard / Pickups
-            <h2>Order Pick-ups</h2>
-            <Box
+            <Paper
                 sx={{
-                    color: "#82692E",
+                    p: 2,
                 }}
             >
+                <Box sx={{ mx: 2 }}>
+                    <Typography fontSize={15} variant="subtitle1" sx={{ mb: 1 }}>
+                    Dashboard / Pickups
+                    </Typography>
+                    <Typography fontSize={30} sx={{ borderBottom: "solid", borderWidth: 2, borderColor: "primary.main", mb: 6, width: "100%" }}>Order Pick-ups</Typography>
+                    <Box
+                        sx={{
+                            color: "#82692E",
+                        }}
+                    >
                 Available for Sale:<strong> 100</strong>
-            </Box>
-            <Tabs orders={orders}/>
+                    </Box>
+                    <Tabs orders={orders}/>
+                </Box>
+            </Paper>
         </Box>
     );
 }
