f061584c4dedf646f361b2fa79679b8ef5b8a4cd
Cole Thiessen <colekthiessen@gmail.com>
2023-05-31 22:02:43 -0600

    feat: fix pickup table actions. lift table comp up. pass is loading data. calculate remaining boxes

--- src/pages/PickupPage.tsx
+++ src/pages/PickupPage.tsx
@@ -2,13 +2,14 @@ import { Dashboard } from "../components/dashboard/Dashboard";
 import React from "react";
 import Pickups from "../components/pickups/Pickups";
 import useGetOrders from "../hooks/useGetOrders";
+import useGetKiwanisTotalBoxes from "../hooks/useGetKiwanisTotalOrders";
 
 function PickupPage() {
-    const { data: orders } = useGetOrders();
+    const { data: orders, isLoading } = useGetOrders();
 
     return (
         <Dashboard>
-            <Pickups orders={orders ?? []}></Pickups>
+            <Pickups orders={orders ?? []} isLoading={isLoading}></Pickups>
         </Dashboard>
     );
 }
