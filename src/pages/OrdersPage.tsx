4020bb3409308c6a39696dbaabd09f540b59b479
Cole Thiessen <colekthiessen@gmail.com>
2023-06-17 19:31:47 -0600

    feat: remove unused component

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Dashboard } from "../components/dashboard/Dashboard";
 import Orders from "../components/orders/Orders";
 import useGetOrders from "../hooks/useGetOrders";
-import StepperProvider from "../providers/StepperProvider";
 
 export default function OrdersPage() {
     const { data, isLoading } = useGetOrders();
 
     return (
         <Dashboard>
-            <StepperProvider>
-                <Orders orders={data ?? []} isLoading={isLoading} />
-            </StepperProvider>
+            <Orders orders={data ?? []} isLoading={isLoading} />
         </Dashboard>
     );
 }
