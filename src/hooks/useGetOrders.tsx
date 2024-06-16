78650d23560cd45ae92795d73cc98b97b66ce88a
Cole Thiessen <colekthiessen@gmail.com>
2024-06-16 16:33:25 -0600

    feat: remove query limit

--- src/hooks/useGetOrders.tsx
+++ src/hooks/useGetOrders.tsx
@@ -1,17 +1,15 @@
 import { db } from "../config";
-import { collection, getDocs, limit, query } from "firebase/firestore";
+import { collection, getDocs, query } from "firebase/firestore";
 import { useQuery } from "@tanstack/react-query";
 import { ORDERS_COLLECTION } from "../constants";
 import { Order } from "../types/Order";
-
-const ORDER_LIMIT = 1000;
 
 const useGetOrders = () => {
     return useQuery({
         queryKey: [ORDERS_COLLECTION],
         queryFn: async () => {
             const clientsRef = collection(db, ORDERS_COLLECTION);
-            const snapshot = await getDocs(query(clientsRef, limit(ORDER_LIMIT)));
+            const snapshot = await getDocs(query(clientsRef));
             const orderObjects = snapshot.docs.map((doc) => {
                 const data = doc.data();
                 return {
